3bbd1d1e04c99fc0e27574c310af5ec3b231180b
jlmortola <jlmortola@me.com>
2020-10-23 16:35:46 +0200

    add: react and nextjs

---
+++ components/SingleImagePaella.jsx
@@ -0,0 +1,9 @@
+const SingleImagePaella = () => {
+  return (
+      <div>
+        <img src="/images/paella-de-gambas-con-chocolate.jpg"/>
+      </div>
+  );
+};
+
+export default SingleImagePaella
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import SingleImagePaella from 'components/SingleImagePaella'
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Paella del copon</h1>
+      <SingleImagePaella />
+    </div>
+  );
+};
+
+export default Index;
